a5895a8f8042fce4a2ffdc51192e016f73bf335e
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-12-22 00:03:25 -0600

    refactors to a Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class ListContacts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map(contact => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{ backgroundImage: `url(${contact.avatarURL})` }}
-            />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className="contact-remove">Remove</button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+const ListContacts = props => (
+  <ol className="contact-list">
+    {props.contacts.map(contact => (
+      <li key={contact.id} className="contact-list-item">
+        <div
+          className="contact-avatar"
+          style={{ backgroundImage: `url(${contact.avatarURL})` }}
+        />
+        <div className="contact-details">
+          <p>{contact.name}</p>
+          <p>{contact.email}</p>
+        </div>
+        <button className="contact-remove">Remove</button>
+      </li>
+    ))}
+  </ol>
+);
 
 export default ListContacts;
