012408560b93f7b94a26fb584691c88bea85eef6
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-01 19:39:25 -0600

    adds onDeleteContact function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -12,7 +12,12 @@ const ListContacts = props => (
           <p>{contact.name}</p>
           <p>{contact.email}</p>
         </div>
-        <button className="contact-remove">Remove</button>
+        <button
+          onClick={() => props.onDeleteContact(contact)}
+          className="contact-remove"
+        >
+          Remove
+        </button>
       </li>
     ))}
   </ol>
