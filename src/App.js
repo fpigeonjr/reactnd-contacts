d8aa2b9811a99deb3dfc6d253f957ca1147a8908
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-02 21:04:10 -0600

    adds Route for homescreen and add contact component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from "./utils/ContactsAPI";
 import CreateContact from "./CreateContact";
 
 class App extends Component {
   state = {
-    screen: "list", //list or create page
     contacts: []
   };
 
@@ -26,16 +26,17 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {this.state.screen === "list" && (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-            onNavigate={() => {
-              this.setState({ screen: "create" });
-            }}
-          />
-        )}
-        {this.state.screen === "create" && <CreateContact />}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              onDeleteContact={this.removeContact}
+              contacts={this.state.contacts}
+            />
+          )}
+        />
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
