96364004e3b937cf2af0b72436b7ad7157cf55d5
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-02 22:05:34 -0600

    serializes form data

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ImageInput from "./ImageInput";
+import SerializeForm from "form-serialize";
 
 class CreateContacts extends Component {
   state = {};
+  handleSubmit(e) {
+    e.preventDefault();
+    const values = SerializeForm(e.target, { hash: true });
+    if (this.props.onCreateContact) this.props.onCreateContact(values);
+  }
   render() {
     return (
       <div>
         <Link to="/" className="close-create-contact">
           Close
         </Link>
-        <form action="" className="create-contact-form">
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name="avatarURL"
